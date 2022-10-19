736dbe094f17d83e1e6b73cc8fd1a70cebfd52df
Martin Steen Andersen <martinsteenandersen@gmail.com>
2022-10-19 21:00:14 +0200

    Enhanced logging.

--- src/utils/logging_helper.py
+++ src/utils/logging_helper.py
@@ -1,12 +1,13 @@
 import logging
+from datetime import datetime as dt
 
 
 def initiate_logger():
     logging.basicConfig(
-        filename="KonverterXML.log",
+        filename=f"KonverterXML_{dt.now().strftime('%Hh%Mm%Ss')}.log",
         filemode='a',
-        format='%(asctime)s,%(msecs)d %(name)s %(levelname)s %(message)s',
-        datefmt='%H:%M:%S',
+        format='%(asctime)s.%(msecs)d %(name)s %(levelname)s %(message)s',
+        datefmt='%Y-%m-%d %H:%M:%S',
         level=logging.INFO
     )
 
